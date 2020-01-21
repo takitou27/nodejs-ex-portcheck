857c29268a30bf84e233ad76ddbd388ac2168476
Takuya Ito <takitou27+gh@gmail.com>
2020-01-21 17:45:52 +0900

    modified helm dir name

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 
 const os = require('os');
 
-app.get('/hostname',
+app.get('/',
         (req, res) => res.send(`this server name = ${os.hostname()}`));
 
-app.listen(8080);
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);

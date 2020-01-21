9b7dd8a645105502c6597e40321afdfcc1f919ce
Takuya Ito <takitou27+gh@gmail.com>
2020-01-21 17:54:58 +0900

    change port 8082

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const os = require('os');
 app.get('/',
         (req, res) => res.send(`this server name = ${os.hostname()}`));
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8082,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
